ec86ee5c19471cabea8cecc979e30c287f263366
Gerard Gunnewijk <gerard.gunnewijk@live.nl>
2020-01-07 16:19:06 +0100

    Refactored MailDispatcher
    - `GeneratedMessage` also adds the attachments, removing this responsibility from `SendMail`. 
    - In `SendMail` use the `GenerateMesssage` method.
    - Refactored html/text body generation to another method for readability.

--- Skyhop.Mail/MailDispatcher.cs
+++ Skyhop.Mail/MailDispatcher.cs
@@ -1,4 +1,4 @@
-﻿using HtmlAgilityPack;
+using HtmlAgilityPack;
 using MimeKit;
 using SkyHop.Mail;
 using System;
@@ -18,50 +18,12 @@ namespace Skyhop.Mail
             _options = options;
         }
 
-        public async Task<MimeMessage> GenerateMessage<T>(T data) where T : MailBase
+        public async Task<MimeMessage> GenerateMessage<T>(T data, MimeEntity[] attachments = default) where T : MailBase
         {
-            var body = await _renderer.GetViewForModel(data);
+            var (htmlBody, textBody) = await GetBody(data);
 
-            body = PreMailer.Net.PreMailer
-                .MoveCssInline(body)
-                .Html;
-
-            data.BodyBuilder.HtmlBody = body;
-
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(body);
-
-            data.BodyBuilder.TextBody = String.Join(" ", doc.DocumentNode.SelectNodes("//text()").Select(q => q.InnerText));
-
-            var message = new MimeMessage();
-
-            message.Body = data.BodyBuilder.ToMessageBody();
-            message.From.Add(_options.DefaultFromAddress);
-
-            return message;
-        }
-
-
-        public async Task SendMail<T>(
-            T data,
-            MailboxAddress from = default,
-            MailboxAddress[] to = default,
-            MailboxAddress[] cc = default,
-            MailboxAddress[] bcc = default,
-            MimeEntity[] attachments = default) where T : MailBase
-        {
-            var body = await _renderer.GetViewForModel(data);
-
-            body = PreMailer.Net.PreMailer
-                .MoveCssInline(body)
-                .Html;
-
-            data.BodyBuilder.HtmlBody = body;
-
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(body);
-
-            data.BodyBuilder.TextBody = string.Join(" ", doc.DocumentNode.SelectNodes("//text()").Select(q => q.InnerText));
+            data.BodyBuilder.HtmlBody = htmlBody;
+            data.BodyBuilder.TextBody = textBody;
 
             if (attachments != default && attachments.Any())
             {
@@ -71,7 +33,23 @@ namespace Skyhop.Mail
                 }
             }
 
-            data.MailMessage.Body = data.BodyBuilder.ToMessageBody();
+            var message = new MimeMessage
+            {
+                Body = data.BodyBuilder.ToMessageBody()
+            };
+
+            return message;
+        }
+
+        public async Task SendMail<T>(
+            T data,
+            MailboxAddress from = default,
+            MailboxAddress[] to = default,
+            MailboxAddress[] cc = default,
+            MailboxAddress[] bcc = default,
+            MimeEntity[] attachments = default) where T : MailBase
+        {
+            var message = await GenerateMessage(data, attachments);
 
             if (from != default || _options.DefaultFromAddress != default) data.MailMessage.From.Add(from ?? _options.DefaultFromAddress);
 
@@ -80,6 +58,22 @@ namespace Skyhop.Mail
             if (bcc != default && bcc.Any()) data.MailMessage.Bcc.AddRange(bcc);
 
             _options.MailSender.Invoke(data.MailMessage);
+        }
+
+        private async Task<(string HtmlBody, string TextBody)> GetBody<T>(T data)
+        {
+            var htmlBody = await _renderer.GetViewForModel(data);
+
+            htmlBody = PreMailer.Net.PreMailer
+                .MoveCssInline(htmlBody)
+                .Html;
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlBody);
+
+            var txtBody = String.Join(" ", doc.DocumentNode.SelectNodes("//text()").Select(q => q.InnerText));
+
+            return (htmlBody, txtBody);
         }
     }
 }
